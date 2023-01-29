67f2da9d644581ad463bb12db62da6f6922e3848
rochelle <khinthanthar2004@gmail.com>
2023-01-29 12:48:56 +0630

    blog

---
+++ app.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "list") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
